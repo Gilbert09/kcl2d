1238580eccd478d9db0a8d15e75b13ec8a5e4c7d
Danial Sadiq Masood <danialsadiqmasood@hotmail.com>
2014-12-04 21:05:18 +0000

    App cannot be started if there is no internet connection.

--- app/src/main/java/com/seg2/kcl2d/HomeScreenActivity.java
+++ app/src/main/java/com/seg2/kcl2d/HomeScreenActivity.java
@@ -2,8 +2,10 @@ package com.seg2.kcl2d;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -16,13 +18,8 @@ public class HomeScreenActivity extends Activity {
     }
 
     public void showMain(View view) {
-        SharedPreferences sharedPreferences = this.getSharedPreferences("sharedPreferences", MODE_PRIVATE);
-        SharedPreferences.Editor preferenceEditor = sharedPreferences.edit();
-
-        preferenceEditor.putBoolean("shouldHideHomeScreen", true);
-        preferenceEditor.apply();
-
         Intent i = new Intent(this, MainActivity.class);
+        //i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
     }
 }
